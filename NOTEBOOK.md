ba7e6a617b1e3ab9492214347083a0e5979ed0e8
ObernhumerMichael <michaelobernhumer@gmail.com>
2024-10-09 17:04:33 +0200

    updated the NOTEBOOK and the README

--- NOTEBOOK.md
+++ NOTEBOOK.md
@@ -1,4 +1,65 @@
-# RSI Scalping
+<!--toc:start-->
+
+- [General](#general)
+  - [Indicators](#indicators)
+  - [Metrics](#metrics)
+- [Strategys](#strategys)
+  - [RSI Scalping](#rsi-scalping)
+    - [RSI Scalping + Candlestick patterns](#rsi-scalping-candlestick-patterns)
+- [Other](#other)
+<!--toc:end-->
+
+# General
+
+## Indicators
+
+- **Trend Indicators** show the direction of the market.
+- **Momentum Indicators** reveal the strength of the price movement.
+- **Volatility Indicators** highlight the intensity of price fluctuations.
+- **Volume Indicators** confirm the strength behind price trends.
+- **Market Strength Indicators** track overall market movement.
+- **Support/Resistance Indicators** identify key price levels.
+
+## Metrics
+
+1. **CAGR (Compound Annual Growth Rate)**
+
+   - **Definition**: CAGR measures the mean annual growth rate of an investment over a specified time period longer than one year.
+   - **Interpretation**: A CAGR of 101.833% means that the investment or portfolio grew at an average rate of 101.833% per year over the period being measured.
+     It shows the smoothed growth rate, ignoring volatility during the period.
+
+1. **Sharpe Ratio**
+
+   - **Definition**: The Sharpe Ratio measures the risk-adjusted return of a portfolio, where risk is defined by standard deviation.
+   - **Interpretation**: A Sharpe Ratio of 2.02 means the portfolio generates 2.02 units of return for every unit of risk.
+     Generally, a Sharpe Ratio greater than 1 is considered good, and above 2 is very good.
+
+1. **Sortino Ratio**
+
+   - **Definition**: The Sortino Ratio is a variation of the Sharpe Ratio, but it differentiates harmful volatility (downside deviation) from total volatility.
+   - **Interpretation**: A Sortino Ratio of 8.61 indicates that the portfolio generates returns 8.61 times higher than the downside risk (negative volatility).
+     A higher ratio implies better risk-adjusted returns, focusing only on downside risk.
+
+1. **Calmar Ratio**
+
+   - **Definition**: The Calmar Ratio is a performance metric that compares the annualized rate of return to the maximum drawdown (the largest peak-to-trough decline).
+   - **Interpretation**: A Calmar Ratio of 61.93 is extremely high, indicating that the returns are significantly larger than the worst drawdown the portfolio experienced.
+     High Calmar ratios suggest strong returns with limited downside.
+
+1. **Expectancy (Ratio)**
+
+   - **Definition**: Expectancy is a measure of the average return per trade, taking both the probability of winning and losing trades into account.
+   - **Interpretation**: An expectancy of 2.41 means that on average, each trade is expected to return 2.41 units of profit.
+
+1. **Profit Factor**
+
+   - **Definition**: The Profit Factor is the ratio of gross profits to gross losses. It helps assess the profitability of a trading system or strategy.
+   - **Interpretation**: A profit factor of 1.395 means that for every unit of risk (loss), the system generates 1.395 units of profit.
+     A profit factor above 1.0 indicates a profitable system, though the higher, the better.
+
+# Strategys
+
+## RSI Scalping
 
 **Result:** loss
 
@@ -24,7 +85,7 @@ $$ Sell: RSI \uparrow 70 $$
 - The strategy produces to much wrong signals in trending situations.
 - Works best in 15m timeframe.
 
-## RSI Scalping + Candlestick patterns
+### RSI Scalping + Candlestick patterns
 
 **Result:** profit
 
@@ -51,4 +112,9 @@ $$RSI \uparrow 70 $$
 
 - Adjusting the limits (eg 20 $\rightarrow$ 30) produces too many false signals.
 - Adding multiple candlestick patterns as triggers has not brought any significant improvement
+
   - But this has to be tested out more in the future on a broader timerange.
+
+  # Other
+
+  _nothing to see here yet_
---
+++ strategies/RSIScalpingVolume.py
@@ -0,0 +1,135 @@
+# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
+# flake8: noqa: F401
+# isort: skip_file
+# --- Do not remove these imports ---
+from operator import length_hint
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta, timezone
+from pandas import DataFrame
+from typing import Dict, Optional, Union, Tuple
+
+from technical.vendor.qtpylib.indicators import crossed_above
+
+from freqtrade.strategy import (
+    IStrategy,
+    Trade,
+    Order,
+    PairLocks,
+    informative,  # @informative decorator
+    # Hyperopt Parameters
+    BooleanParameter,
+    CategoricalParameter,
+    DecimalParameter,
+    IntParameter,
+    RealParameter,
+    # timeframe helpers
+    timeframe_to_minutes,
+    timeframe_to_next_date,
+    timeframe_to_prev_date,
+    # Strategy helper functions
+    merge_informative_pair,
+    stoploss_from_absolute,
+    stoploss_from_open,
+)
+
+# --------------------------------
+# Add your lib to import here
+import talib.abstract as ta
+import pandas_ta as pta
+from technical import qtpylib
+
+# --------------------------------
+# Backtesting
+from functools import reduce
+
+
+class RSI_SCALPING_VOLUME(IStrategy):
+    # Strategy interface version - allow new iterations of the strategy interface.
+    # Check the documentation or the Sample strategy to get the latest version.
+    INTERFACE_VERSION = 3
+
+    # Optimal timeframe for the strategy.
+    timeframe = "1m"
+
+    # Can this strategy go short?
+    can_short: bool = False
+
+    # Minimal ROI designed for the strategy.
+    # This attribute will be overridden if the config file contains "minimal_roi".
+    minimal_roi = {"0": 0.5}
+
+    # Optimal stoploss designed for the strategy.
+    # This attribute will be overridden if the config file contains "stoploss".
+    stoploss = -0.5
+
+    # Trailing stoploss
+    # trailing_stop = True
+    # trailing_only_offset_is_reached = True
+    # trailing_stop_positive = 0.003
+    # trailing_stop_positive_offset = 0.005  # Disabled / not configured
+
+    # Run "populate_indicators()" only for new candle.
+    process_only_new_candles = True
+
+    # These values can be overridden in the config.
+    use_exit_signal = True
+    exit_profit_only = False
+    ignore_roi_if_entry_signal = False
+
+    # Number of candles the strategy requires before producing valid signals
+    startup_candle_count: int = 50
+
+    # Define the parameter spaces
+    rsi = IntParameter(2, 20, default=14)
+
+    order_types = {
+        "entry": "limit",
+        "exit": "limit",
+        "stoploss": "market",
+        "stoploss_on_exchange": False,
+    }
+
+    plot_config = {
+        # Main plot indicators (Moving averages, ...)
+        "main_plot": {},
+        "subplots": {
+            # Subplots - each dict defines one additional plot
+            "RSI": {
+                f"rsi": {"color": "red"},
+            },
+        },
+    }
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """Generate all indicators used by the strategy"""
+
+        # RSI
+        dataframe[f"rsi"] = pta.rsi(dataframe["close"], length=self.rsi.value)
+
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        conditions_long = []
+
+        conditions_long.append(dataframe["rsi"] < 20)
+
+        # Check that volume is not 0
+        conditions_long.append(dataframe["volume"] > 0)
+
+        if conditions_long:
+            dataframe.loc[reduce(lambda x, y: x & y, conditions_long), "enter_long"] = 1
+
+        return dataframe
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        conditions_long = []
+
+        conditions_long.append((dataframe["rsi"] > 70))
+
+        # Check that volume is not 0
+        conditions_long.append(dataframe["volume"] > 0)
+
+        if conditions_long:
+            dataframe.loc[reduce(lambda x, y: x & y, conditions_long), "exit_long"] = 1
+        return dataframe
